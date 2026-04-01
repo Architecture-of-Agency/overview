308b9ae0417f72a968febcea443dc61c393095f9
LDunhill <85881403+LDunhill@users.noreply.github.com>
2026-04-01 03:29:35 +0100

    Delete scripts directory

--- scripts/data-processing/data_processor.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Neighborhood Data Processor"""
-
-import pandas as pd
-
-print("🎓 PhD Urban Governance Data Processor")
-print("Ready to process your neighborhood data!")
-
-# Your data processing code goes here
